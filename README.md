337141e4e04bc95d2f363c1af0bd36013cd74b81
Kacper Bielak <github@bielak2000.com>
2025-07-15 22:36:05 +0200

    Init project and add flayway files and configuration

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# users_managment
+# Users management
+
+The project is used in other projects to manage user experience. It creates appropriate database tables representing
+users, notification settings, addresses and companies in the relation database - PostgreSQL.
+
+## Integration
+
+Add dependency to `pom.xml` file as below:
+
+```
+<dependency>
+  <groupId>users.management</groupId>
+  <artifactId>UsersManagement</artifactId>
+  <version>${users-management-version}</version>
+</dependency>
+```
+
+NOTE! Remember to start flaway files in your project with 'V1...'.
---
+++ src/main/resources/db.migration/V0.1__init_users_tables.sql
@@ -0,0 +1,61 @@
+create schema if not exists users;
+
+CREATE TABLE users.address
+(
+    id               uuid PRIMARY KEY,
+    town             VARCHAR(100) NOT NULL,
+    postal_code      VARCHAR(10)  NOT NULL,
+    house_number     VARCHAR(20)  NOT NULL,
+    apartment_number VARCHAR(20),
+    city             VARCHAR(100) NOT NULL,
+    street           VARCHAR(100) NOT NULL,
+    created_at       DATE         NOT NULL,
+    modified_at      DATE         NOT NULL,
+    deleted          boolean      NOT NULL
+);
+
+CREATE TABLE users.company
+(
+    id                 uuid PRIMARY KEY,
+    address_id         uuid         not null,
+    name               VARCHAR(100) NOT NULL,
+    tax_identification VARCHAR(20)  NOT NULL,
+    email              VARCHAR(100) NOT NULL,
+    phone_number       VARCHAR(12),
+    created_at         DATE         NOT NULL,
+    modified_at        DATE         NOT NULL,
+    deleted            boolean      NOT NULL,
+    CONSTRAINT fk_address FOREIGN KEY (address_id) REFERENCES users.address (id)
+);
+
+CREATE TABLE users.users
+(
+    id               uuid PRIMARY KEY,
+    company_id       uuid,
+    address_id       uuid,
+    name             VARCHAR(100) NOT NULL,
+    surname          VARCHAR(100) NOT NULL,
+    email            VARCHAR(100) NOT NULL,
+    phone_number     VARCHAR(12),
+    password         VARCHAR(200) NOT NULL,
+    role             VARCHAR(100) NOT NULL,
+    created_at       DATE         NOT NULL,
+    modified_at      DATE         NOT NULL,
+    last_activity_at DATE         NOT NULL,
+    deleted          boolean      NOT NULL,
+    CONSTRAINT fk_company FOREIGN KEY (company_id) REFERENCES users.company (id),
+    CONSTRAINT fk_address FOREIGN KEY (address_id) REFERENCES users.address (id)
+);
+
+CREATE TABLE users.user_settings
+(
+    id                 uuid PRIMARY KEY,
+    user_id            uuid    NOT NULL,
+    sms_notification   boolean NOT NULL,
+    app_notification   boolean NOT NULL,
+    email_notification boolean NOT NULL,
+    created_at         DATE    NOT NULL,
+    modified_at        DATE    NOT NULL,
+    deleted            boolean NOT NULL,
+    CONSTRAINT fk_users FOREIGN KEY (user_id) REFERENCES users.users (id)
+);
